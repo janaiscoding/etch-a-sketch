2fff3415dd5c5dba300705ac3174aec6394946da
JanaIsCoding <jana.istrate@gmail.com>
2022-11-12 01:07:27 +0200

    Add mouseover effect

--- javascript.js
+++ javascript.js
@@ -1,21 +1,26 @@
+const currentColor = '#333333';
+
 (function createSketchboard(){
 
 function createOneDiv() {
-    let smallDiv = document.createElement('div');
-    smallDiv.className = "small-div";
-    smallDiv.innerText = "div";
-    return smallDiv;
+    let gridElement = document.createElement('div');
+    gridElement.className = "grid-element";
+    gridElement.innerText = "div";
+    gridElement.addEventListener("mouseover", changeColor);
+    return gridElement;
 }
 
 function createAllDivs() {
-    let container = document.querySelector('.container');
-    myDivs = [];
-    i = 0;
-    numOfDivs = 256;
-    for (i; i < numOfDivs; i += 1) {
-        myDivs.push(createOneDiv());
-        container.appendChild(myDivs[i]);
+    let grid = document.querySelector('.grid');
+    smallDivs = [];
+    for (let i = 0; i < 256; i += 1) {
+        smallDivs.push(createOneDiv());
+        grid.appendChild(smallDivs[i]);
       }
 }
 createAllDivs();
 }());
+
+function changeColor(e){
+    e.target.style.backgroundColor = currentColor;
+}

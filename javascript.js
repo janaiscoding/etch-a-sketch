b091bfdfd9db1a461e941972f4dcb28478aeec4c
JanaIsCoding <jana.istrate@gmail.com>
2022-11-12 00:13:12 +0200

    Rename function names for clear intent

--- javascript.js
+++ javascript.js
@@ -1,22 +1,21 @@
 (function createSketchboard(){
 
-function createDiv() {
+function createOneDiv() {
     let smallDiv = document.createElement('div');
     smallDiv.className = "small-div";
     smallDiv.innerText = "div";
     return smallDiv;
 }
 
-function rowDivs() {
+function createAllDivs() {
     let container = document.querySelector('.container');
     myDivs = [];
     i = 0;
     numOfDivs = 256;
     for (i; i < numOfDivs; i += 1) {
-        myDivs.push(createDiv());
+        myDivs.push(createOneDiv());
         container.appendChild(myDivs[i]);
       }
-    
 }
-rowDivs();
+createAllDivs();
 }());

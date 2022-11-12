16699338d21a1cd4b6ffac59eb3aac1a6192b610
JanaIsCoding <jana.istrate@gmail.com>
2022-11-12 13:07:30 +0200

    Create changing sizes functions

--- javascript.js
+++ javascript.js
@@ -1,26 +1,54 @@
-const currentColor = '#333333';
+// all variables
+const defaultColor = '#333333';
+const defaultSize = 16;
+const button = document.querySelector("button");
+const grid = document.querySelector(".grid");
+let currentColor = defaultColor;
+let currentSize = defaultSize;
 
-(function createSketchboard(){
 
-function createOneDiv() {
-    let gridElement = document.createElement('div');
-    gridElement.className = "grid-element";
-    gridElement.innerText = "div";
+// creating the og board
+function createSketchboard(size){
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i ++) {
+    const gridElement = document.createElement('div');
     gridElement.addEventListener("mouseover", changeColor);
-    return gridElement;
+    gridElement.addEventListener("mousedown",changeColor);
+    gridElement.className = "grid-element";
+    gridElement.innerText = "ONE";
+    grid.appendChild(gridElement);
+    }
 }
 
-function createAllDivs() {
-    let grid = document.querySelector('.grid');
-    smallDivs = [];
-    for (let i = 0; i < 256; i += 1) {
-        smallDivs.push(createOneDiv());
-        grid.appendChild(smallDivs[i]);
-      }
+
+function getCurrentSize() {
+    let currentSize = window.prompt("Please specify new size:");
+    grid.innerHTML = '';
+    if (currentSize>50){
+        alert ("Its too much! Max 50!");
+        createSketchboard(defaultSize);
+    }
+    else if (currentSize === null) {
+        createSketchboard(defaultSize);
+    }
+    else {
+        createSketchboard(currentSize);
+    }
+    console.log(currentSize);
 }
-createAllDivs();
-}());
+
+
+function clearSketchboard() {
+    grid.innerHTML = '';
+    if (grid.innerHTML === ''){
+        createSketchboard(defaultSize);
+    }
+}
 
 function changeColor(e){
     e.target.style.backgroundColor = currentColor;
 }
+
+createSketchboard(defaultSize);
